834b238acb72ed6cac9ad06b5ae887ab4eaa0c4f
septianpramana <ianpaolin@gmail.com>
2017-06-08 00:35:30 +0700

    sejarah edit

--- application/views/content/profil/sejarah.php
+++ application/views/content/profil/sejarah.php
@@ -1,44 +1,94 @@
 <div class="wrapper">
-    <div class="inner light">
+	<div class="inner light">
 		<div class="page-intro clearfix">
-		<div class="container" style="margin-bottom: 15px;">
-		<h1><strong>Sejarah SMP Negeri 2 Sungai Penuh</h3></Strong>
-		<hr>
-
-<div class="hr1"></div>
-	<div class="one-half">
-			<p><img src="https://ret-tech.id/SMPNDEV/SMPN2/images/header/1.jpg" width="600" height="300"/></p></div>
-<div class="">
-<p style="text-align:justify;"><span class="dropcap">S</span>SMP Negeri 2 Sungai Penuh sejak berdiri tanggal 25 Mei 1960 bernama SGB sampai berubah menjadi SMP Negeri 2 Sungai Penuh, 
-				Sejak berdirinya Tahun 1960 sampai sekarang SMP Negeri 2 Sungai Penuh  berkedudukan di Sungai Penuh ibu kota Kabupaten Kerinci sekarang berubah nama Kota Sungai Penuh, tepatnya di Jl.Jend. Sudirman  Kecamatan Sungai Penuh Kota Sungai Penuh. 
-				Berdirinya SMP Negeri 2 Sungai Penuh ini atas kerjasama seluruh masyarakat Kecamatan Sungai Penuh untuk mendesak Pemerintah Daerah Tingkat II Kabupaten Kerinci, 
-				karena masyarakat memandang bahwa Sekolah Lanjutan Tingkat Pertama di Kabupaten Kerinci tidak akan mungkin dapat menampung semua tamatan Sekolah Dasar, pada tahun 1960, Tamatan Sekolah Dasar memang sangat banyak. 
-				Jadi cukup beralasan ketika masyarakat mengharap adanya Sekolah Lanjutan Tingkat Pertama di Kecamatan Sungai Penuh.
-				SMP Negeri 2 Sungai Penuh kini telah mengalami beberapa kemajuan yang sangat signifikan, SMP Negeri Sungai Penuh telah memiliki XXIV Ruang Kelas, X Ruang Lab, dan Lapangan Olah Raga. 
-				Dalam Perjalanan SMP Negeri 2 Sungai Penuh yang berdiri tahun 1960 terus berbenah dibawah kepemimpinan kepala sekolah yang telah beberapa kali mengalami pergantian hingga kini telah memiliki 9 (sembilan) orang kepala sekolah. 
-				Kepala Sekolah tersebut sama-sama mempunyai visi dan misi dalam membangun SMP Negeri 2 Sungai Penuh kearah kemajuan dalam mencapai tujuan pendidikan nasional sesuai dengan undang-undang dasar 1945 yaitu "untuk mencerdaskan kehidupan bangsa."</div>
-<h5>Kepala Sekolah SMP Negeri 2 Sungai Penuh 9 Periode Terakhir:</h5>
-		<ol class="">
-				<li>A.Noerwan, BA (Periode Jabatan Tahun 1960-1964).</li>
-				<li>Hj.Syarifah Bukhari (Periode Jabatan Tahun 1964-1985).</li>
-				<li>Mansyurdin (Periode Jabatan Tahun 1985-1988).</li>
-				<li>Abd.Manaf (Periode Jabatan Tahun 1988-1996)</li>
-				<li>Drs.Johar Maknum (Periode Jabatan Tahun 1996-2004)</li>
-				<li>Drs.Pagaruyung Harahap (Periode Jabatan Tahun 2004-2008)</li>
-				<li>Hendripal, S.Pd (Periode Jabatan Tahun 2008-2011)</li>
-				<li>Febrina Yusvanelly, S.Pd (Periode Jabatan Tahun 2011-2012)</li>
-				<li>Misyenni, S.Pd (Periode Jabatan Tahun 2012-Sekarang)</li>
-			</ol>
-				<p>Dilihat dari letak geografisnya SMP Negeri 2 Sungai Penuh sangat lah strategis, karena SMP Negeri 2 Sungai Penuh berada di pusat Ibu Kota.</p>
-		<h5>SMP Negeri 2 Sungai Penuh berdasarkan batas-batas bangunannya:</h5>
-		<ol class="">
-				<li>Sebelah barat berbatasaan dengan Persawahan.</li>
-				<li>Sebelah timur berbatasaan dengan Jalan Raya Jend. Sudirman.</li>
-				<li>Sebelah utara berbatasaan dengan Dinas Ketahanan Pangan.</li>
-				<li>Sebelah selatan berbatasaan dengan Bazda Kota Sungai Penuh.</li>
-			</ol>
-			
-</div>
-	</div>
-		</div>
+			<div class="container" style="margin-bottom: 15px;">
+				<h1><strong>Sejarah SMP Negeri 2 Sungai Penuh</h3></Strong>
+				<hr>
+<!-- Begin Sidebar -->
+				<aside class="sidebar">
+					<div class="sidebox widget">
+						<h3>Popular Posts</h3>
+						<ul class="post-list">
+							<li>
+								<h6><a href="blog-post.html">Vivamus sagittis lacus vel augue metus laoreet rutrum dolor auctor.</a></h6>
+								<em>3th Oct 2012</em> </li>
+							<li>
+								<h6><a href="blog-post.html">Scelerisque nisl consectetur et.</a></h6>
+								<em>28th Sep 2012</em> </li>
+							<li>
+								<h6><a href="blog-post.html">Pellentesque ornare sem lacinia quam venenatis vestibulum.</a></h6>
+								<em>15th Aug 2012</em> </li>
+						</ul>
+					</div>
+						<div class="sidebox widget">
+							<h3>Search</h3>
+							<form class="searchform" method="get">
+								<input type="text" id="s2" name="s" value="type and hit enter" onfocus="this.value=''" onblur="this.value='type and hit enter'"/>
+							</form>
+						</div>
+					<div class="sidebox widget">
+						<h3>Custom Text</h3>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+						<p>Sed posuere consectetur est at lobortis. Vivamus sagittis lacus vel augue. Fusce  mauris condimentum.</p>
+					</div>
+						<div class="sidebox widget">
+							<h3>Categories</h3>
+							<ul class="list">
+								<li><a href="#">Web Design</a> (21)</li>
+								<li><a href="#">Photography</a> (19)</li>
+								<li><a href="#">Graphic Design</a> (16)</li>
+								<li><a href="#">Manipulation</a> (15)</li>
+								<li><a href="#">Motion Graphics</a> (12)</li>
+							</ul>
+						</div>
+				</aside>
+		<!-- End Sidebar -->
+		
+		<section class="content">
+				<div class="portfoliohalf-bannercontainer">
+					<div class="portfoliohalf-banner">
+						<ul>
+							<li data-transition="random"><img src="https://ret-tech.id/SMPNDEV/SMPN2/images/header/1.jpg" alt="" /> </li>
+							<li data-transition="random"><img src="https://ret-tech.id/SMPNDEV/SMPN2/images/header/2.jpg" alt="" /> </li>
+							<li data-transition="random"><img src="https://ret-tech.id/SMPNDEV/SMPN2/images/header/3.jpg" alt="" /> </li>
+						</ul>
+						<div class="tp-bannertimer tp-bottom"></div>
+					</div>
+				</div>
+		</section>
 			</div>
+				<div class="four-fifth">
+					<p style="text-align:justify;"><span class="dropcap">S</span>SMP Negeri 2 Sungai Penuh sejak berdiri tanggal 25 Mei 1960 bernama SGB sampai berubah menjadi SMP Negeri 2 Sungai Penuh, 
+						Sejak berdirinya Tahun 1960 sampai sekarang SMP Negeri 2 Sungai Penuh  berkedudukan di Sungai Penuh ibu kota Kabupaten Kerinci sekarang berubah nama Kota Sungai Penuh, tepatnya di Jl.Jend. Sudirman  Kecamatan Sungai Penuh Kota Sungai Penuh. 
+						Berdirinya SMP Negeri 2 Sungai Penuh ini atas kerjasama seluruh masyarakat Kecamatan Sungai Penuh untuk mendesak Pemerintah Daerah Tingkat II Kabupaten Kerinci, 
+						karena masyarakat memandang bahwa Sekolah Lanjutan Tingkat Pertama di Kabupaten Kerinci tidak akan mungkin dapat menampung semua tamatan Sekolah Dasar, pada tahun 1960, Tamatan Sekolah Dasar memang sangat banyak. 
+						Jadi cukup beralasan ketika masyarakat mengharap adanya Sekolah Lanjutan Tingkat Pertama di Kecamatan Sungai Penuh.
+						SMP Negeri 2 Sungai Penuh kini telah mengalami beberapa kemajuan yang sangat signifikan, SMP Negeri Sungai Penuh telah memiliki XXIV Ruang Kelas, X Ruang Lab, dan Lapangan Olah Raga. 
+						Dalam Perjalanan SMP Negeri 2 Sungai Penuh yang berdiri tahun 1960 terus berbenah dibawah kepemimpinan kepala sekolah yang telah beberapa kali mengalami pergantian hingga kini telah memiliki 9 (sembilan) orang kepala sekolah. 
+						Kepala Sekolah tersebut sama-sama mempunyai visi dan misi dalam membangun SMP Negeri 2 Sungai Penuh kearah kemajuan dalam mencapai tujuan pendidikan nasional sesuai dengan undang-undang dasar 1945 yaitu "untuk mencerdaskan kehidupan bangsa."
+					<h5>Kepala Sekolah SMP Negeri 2 Sungai Penuh 9 Periode Terakhir:</h5>
+					<ol class="">
+						<li>A.Noerwan, BA (Periode Jabatan Tahun 1960-1964).</li>
+						<li>Hj.Syarifah Bukhari (Periode Jabatan Tahun 1964-1985).</li>
+						<li>Mansyurdin (Periode Jabatan Tahun 1985-1988).</li>
+						<li>Abd.Manaf (Periode Jabatan Tahun 1988-1996)</li>
+						<li>Drs.Johar Maknum (Periode Jabatan Tahun 1996-2004)</li>
+						<li>Drs.Pagaruyung Harahap (Periode Jabatan Tahun 2004-2008)</li>
+						<li>Hendripal, S.Pd (Periode Jabatan Tahun 2008-2011)</li>
+						<li>Febrina Yusvanelly, S.Pd (Periode Jabatan Tahun 2011-2012)</li>
+						<li>Misyenni, S.Pd (Periode Jabatan Tahun 2012-Sekarang)</li>
+					</ol>
+					<p>Dilihat dari letak geografisnya SMP Negeri 2 Sungai Penuh sangat lah strategis, karena SMP Negeri 2 Sungai Penuh berada di pusat Ibu Kota.</p>
+					<h5>SMP Negeri 2 Sungai Penuh berdasarkan batas-batas bangunannya:</h5>
+					<ol class="">
+						<li>Sebelah barat berbatasaan dengan Persawahan.</li>
+						<li>Sebelah timur berbatasaan dengan Jalan Raya Jend. Sudirman.</li>
+						<li>Sebelah utara berbatasaan dengan Dinas Ketahanan Pangan.</li>
+						<li>Sebelah selatan berbatasaan dengan Bazda Kota Sungai Penuh.</li>
+					</ol>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</div>
