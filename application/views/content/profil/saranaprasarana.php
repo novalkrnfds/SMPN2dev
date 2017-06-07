962593f170ce8e84545954ad8a2e4ef238480b7a
SeptianPS <iansntzer@gmail.com>
2017-06-07 20:59:36 +0700

    add sejarah

--- application/views/content/profil/saranaprasarana.php
+++ application/views/content/profil/saranaprasarana.php
@@ -6,28 +6,4 @@
 		<hr>
 <p style="text-align: left;"><b> </b><br />
 <span style="color: #000000;"><strong>A. SARANA SUMBER BELAJAR (Ruang Kelas)</strong></span></p>
-<table border="1" width="90%">
-<tbody>
-<tr align="center">
-<td style="text-align: center;" rowspan="2"><span style="color: #000000;">KONDISI</span></td>
-<td style="text-align: center;" rowspan="2"><span style="color: #000000;">Jumlah ruang yg digunakan untuk Ruang Kelas(f)=(d+e)</span></td>
-<td style="text-align: center;" rowspan="2"><span style="color: #000000;">Jml. ruang lainnya yg digunakan untuk r. Kelas(e)</span></td>
-<td style="text-align: center;" rowspan=""><span style="color: #000000;">Jumlah dan ukuran</span></td>
-</tr>
-<tr align="center">
-<td style="text-align: center;"><span style="color: #000000;">Ukuran 7x9 m2(a)</span></td>
-<td style="text-align: center;"><span style="color: #000000;">Ukuran > 63m2(b)</span></td>
-<td style="text-align: center;"><span style="color: #000000;">Ukuran < 63 m2(c)</span></td>
-<td style="text-align: center;"><span style="color: #000000;">Jumlah(d)=(a+b+c)</span></td>
-<tr>
-<td><span style="color: #000000;">Baik</span></td>
-<td style="text-align: left;"><span style="color: #000000;">&#8211;</span></td>
-<td style="text-align: center;"><span style="color: #000000;">&#8211;</span></td>
-<td><span style="color: #000000;">8&#215;15 m</span></td>
-<td><span style="color: #000000;">Baik</span></td>
-<td><span style="color: #000000;">&#8211;</span></td>
-<td><span style="color: #000000;">&#8211;</span></td>
-</tr>
-</tr>
-</div>
-</div>
+<
